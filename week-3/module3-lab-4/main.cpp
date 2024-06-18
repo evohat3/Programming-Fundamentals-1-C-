bfae675bf9d61200109a52c498ff8da125e8379a
evohat3 <118646438+evohat3@users.noreply.github.com>
2024-06-17 21:01:01 -0500

    finished module3-lab4

--- week-3/module3-lab-4/main.cpp
+++ week-3/module3-lab-4/main.cpp
@@ -10,65 +10,91 @@ using namespace std;
 int main() {
     double numOne;
     double numTwo;
-    int choice;
+    int choice = -1;
     double result;
 
-    cout << "CALCULATOR MENU" << endl;
+
+    while (choice != 0) {
+
+
+    cout << "*** CALCULATOR MENU ***" << endl;
     cout << "1. Add" << endl;
     cout << "2. Subtract" << endl;
     cout << "3. Multiply" << endl;
     cout << "4. division" << endl;
     cout << "0. Exit" << endl;
+    cout << "******************************************************" << endl;
 
-    cout << " " << endl;
-    cout << "Make a selection from 1 to 4 and select 0 to exit." << endl;
-    cin >> choice;
+    cout << "Type the number of the operation you want from 1 to 4 " << endl; 
+    cout << "or select 0 to exit the application." << endl;
+    cout << "******************************************************" << endl;
+    cout << "---> Operation selection: "; cin >> choice;
 
     switch (choice)
     {
     case 1:
-        cout << "Enter the 1st number: ";
+        cout << "Enter the 1st number to add: " << endl;
         cin >> numOne; 
-        cout << "Enter the 2nd number: ";
+        cout << "Enter the 2nd number to add: " << endl;
         cin >> numTwo;
         result = numOne + numTwo;
-
-        cout << numOne << " + " << numTwo << " = " << result;
+        cout << " " << endl;
+        cout << "*** Your Answer is: ***" << endl;
+        cout << numOne << " + " << numTwo << " = " << result << endl;
+        cout << " " << endl;
 
         break;
     case 2: 
-        cout << "Enter the 1st number: ";
+        cout << "Enter the 1st number to subtract: ";
         cin >> numOne; 
-        cout << "Enter the 2nd number: ";
+        cout << "Enter the 2nd number to subtract: ";
         cin >> numTwo;
         result = numOne - numTwo;
-        cout << numOne << " - " <<  numTwo << " = " << result;
+        cout << " " << endl;
+        cout << "*** Your Answer is: ***" << endl;
+        cout << numOne << " - " <<  numTwo << " = " << result << endl;
+        cout << " " << endl;
         break;
     case 3:
-        cout << "Enter the 1st number: ";
+        cout << "Enter the 1st number to multiply: ";
         cin >> numOne; 
-        cout << "Enter the 2nd number: ";
+        cout << "Enter the 2nd number to multiply: ";
         cin >> numTwo;
         result = numOne * numTwo;
-        cout << numOne << " * " <<  numTwo << " = " << result;
+        cout << " " << endl;
+        cout << "*** Your Answer is: ***" << endl;
+        cout << numOne << " * " <<  numTwo << " = " << result << endl;
+        cout << " " << endl;
         break;
     case 4:
-        cout << "Enter the 1st number: ";
+        cout << "Enter the 1st number to divide: ";
         cin >> numOne; 
-        cout << "Enter the 2nd number: ";
+        cout << "Enter the 2nd number to divide: ";
         cin >> numTwo;
         result = numOne / numTwo;
-        cout << numOne << " / " <<  numTwo << " = " << result;
+        cout << " " << endl; 
+        cout << "*** Your Answer is: ***" << endl;
+        cout << numOne << " / " <<  numTwo << " = " << result << endl;
+        cout << " " << endl;
         break;
-    case 0:
-        cout << " Exiting application. Good bye! " << endl;
-    break;
-    
+    case 0: 
+
+        cout << " " << endl;
+        cout << "*** Exiting application now... Good Bye! ***" << endl;
+        cout << " " << endl;
+        break;
+
     default:
         if (choice >=5) {
+            cout << " " << endl;
             cout << " Invalid choice, please choose either 1-4 or select 0 to exit.";}
+            cout << " " << endl;
         break;
+    } 
+
     }
+
+
     
     return 0;
 }
